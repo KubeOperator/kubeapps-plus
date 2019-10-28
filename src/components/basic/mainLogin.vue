96664d840906190dfc9504168050ce12233c3c7b
OrangeM21 <orangemtony@gmail.com>
2019-10-28 18:33:18 +0800

    fixed

--- src/components/basic/mainLogin.vue
+++ src/components/basic/mainLogin.vue
@@ -1,18 +1,43 @@
 <template>
-    <div 
-    class="main_login"
-    >mainLogin</div>
+  <div class="main_login">
+    <el-card class="box-card">
+      
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
   name: "mainLogin"
-
 };
 </script>
 
 <style scoped>
-.main_login{
-  height:calc(100vh - 120px);
+.main_login {
+  height: calc(100vh - 120px);
 }
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    position: relative;
+    top:calc(50% - 120px);
+    left:calc(50vw - 240px);
+    height: 240px;
+    width: 480px;
+  }
 </style>

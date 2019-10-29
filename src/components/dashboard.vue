2392cfaaa1a25359dfe8ed2222631935e0af3cfc
OrangeM21 <orangemtony@gmail.com>
2019-10-29 16:45:27 +0800

    fixed

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,19 +1,41 @@
 <template>
-<div>dashboard</div>
+  <div>
+    <el-row style="margin-top:10px">
+      <el-col :span="6">
+        <h1 class="app_title">Applications</h1>
+      </el-col>
+      <el-col :span="4">
+        <div style="margin: 1.2em 0;">
+          <el-input placeholder="请输入内容" v-model="input4">
+            <i slot="prefix" class="el-input__icon el-icon-search"></i>
+          </el-input>
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div style="margin: 1.8em 0;">
+          <el-switch v-model="radio" active-text="显示已删除的应用" inactive-text></el-switch>
+        </div>
+      </el-col>
+      <el-col :span="4" :offset="4">
+        <div style="margin: 1.2em 0;">
+             <el-button type="primary">新建应用</el-button>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "doshboard",
-  data(){
+  name: "dashboard",
+  data() {
     return {
-      input:''
-    }
+      radio: ""
+    };
   }
 };
 </script>
 <style scoped>
-
-
-
+.app_title {
+}
 </style>

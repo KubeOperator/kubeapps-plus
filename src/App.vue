f6260e8d86b9ec388571d86d7728ab7704ef4ebd
OrangeM21 <orangemtony@gmail.com>
2019-10-29 14:20:45 +0800

    fixed

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,7 @@
         <basicHeader></basicHeader>
       </el-header>
       <el-main>
-        <mainLogin>
-
-        </mainLogin>
+        <router-view></router-view>
       </el-main>
       <el-footer>
         <basicFooter></basicFooter>
@@ -17,15 +15,13 @@
 </template>
 
 <script>
-import basicHeader from "./components/basic/basicHeader.vue"
-import mainLogin from "./components/basic/mainLogin.vue"
-import basicFooter from "./components/basic/basicFooter.vue"
+import basicHeader from "./components/basic/basicHeader.vue";
+import basicFooter from "./components/basic/basicFooter.vue";
 export default {
   name: "app",
   components: {
     basicHeader,
-    basicFooter,
-    mainLogin
+    basicFooter
   }
 };
 </script>
@@ -33,13 +29,14 @@ export default {
 <style>
 @import "./assets/css/normalize.css";
 @font-face {
-  font-family: 'iconfont';  /* project id 1481498 */
-  src: url('//at.alicdn.com/t/font_1481498_z0wwcntpqo9.eot');
-  src: url('//at.alicdn.com/t/font_1481498_z0wwcntpqo9.eot?#iefix') format('embedded-opentype'),
-  url('//at.alicdn.com/t/font_1481498_z0wwcntpqo9.woff2') format('woff2'),
-  url('//at.alicdn.com/t/font_1481498_z0wwcntpqo9.woff') format('woff'),
-  url('//at.alicdn.com/t/font_1481498_z0wwcntpqo9.ttf') format('truetype'),
-  url('//at.alicdn.com/t/font_1481498_z0wwcntpqo9.svg#iconfont') format('svg');
+  font-family: "iconfont"; /* project id 1481498 */
+  src: url("//at.alicdn.com/t/font_1481498_z0wwcntpqo9.eot");
+  src: url("//at.alicdn.com/t/font_1481498_z0wwcntpqo9.eot?#iefix")
+      format("embedded-opentype"),
+    url("//at.alicdn.com/t/font_1481498_z0wwcntpqo9.woff2") format("woff2"),
+    url("//at.alicdn.com/t/font_1481498_z0wwcntpqo9.woff") format("woff"),
+    url("//at.alicdn.com/t/font_1481498_z0wwcntpqo9.ttf") format("truetype"),
+    url("//at.alicdn.com/t/font_1481498_z0wwcntpqo9.svg#iconfont") format("svg");
 }
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
@@ -49,13 +46,13 @@ export default {
   color: #2c3e50;
   /* margin-top: 60px; */
 }
-.el-main{
+.el-main {
   padding: inherit !important;
 }
-.el-header{
+.el-header {
   padding: inherit !important;
 }
-.el-footer{
+.el-footer {
   padding: inherit !important;
 }
 </style>

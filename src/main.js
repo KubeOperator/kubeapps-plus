3b07af07addf2c84094e713e0168faef941115eb
OrangeM21 <orangemtony@gmail.com>
2019-10-30 16:17:12 +0800

    fixed

--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import './plugins/element.js'
 import router from './route/router.js'
 import VueRouter from 'vue-router'
 import i18n from './i18n/i18n'
+import Resource from 'vue-resource'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
-
+Vue.use(Resource)
 new Vue({
   render: h => h(App),
   router,
---
+++ vue.config.js
@@ -0,0 +1,22 @@
+//vue.config.js
+module.exports = {
+    devServer: {
+        port: 9090,
+
+        host: "localhost",
+
+        https: false,
+
+        open: false,
+        proxy: {
+            "/api": {
+                target: "http://localhost:8080", //设置调用的接口域名和端口
+                changeOrigin: true, //是否跨域
+                ws: true,
+                pathRewrite: {
+                    "^/api": ""
+                }
+            }
+        }
+    }
+}

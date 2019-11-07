50cfe7cc91fa721570ebb3f34712f1f2a3c24609
maguohao2018 <guohao.ma@fit2cloud.com>
2019-11-07 15:11:41 +0800

    utils

---
+++ src/components/utils/searchImage.js
@@ -0,0 +1,14 @@
+/**
+ * @value example: /api/chartsvc/v1/assets/stable/aerospike/logo
+ * @return example: https://hub.kubeapps.com/api/chartsvc/v1/assets/stable/aerospike/logo
+ * @author: maguohao
+ */
+
+const https = 'https://hub.kubeapps.com/api/chartsvc'
+export function searchImage (value) {
+    if (!value) {
+        return ''
+    }
+    let src = https + value
+    return src
+}

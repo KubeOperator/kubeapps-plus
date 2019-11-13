6f97d380a60444ba65bdbfe229365317d159d763
maguohao2018 <guohao.ma@fit2cloud.com>
2019-11-13 18:27:44 +0800

    封装utc 转gmt 组件

---
+++ src/components/utils/UTC2GMT.js
@@ -0,0 +1,24 @@
+/**
+ * 将UTC时间转化为当地时区时间（UTC转GMT）
+ * time: UTC时间格式一般为 "2019-11-16T05:23:20.000Z"
+ * @author: maguohao
+ */
+var UTC2GMT = function (time) {
+    alert(time)
+    if(!time){
+        return '-';
+    }
+    function formatFunc(str) {    //格式化显示
+        return str > 9 ? str : '0' + str
+    }
+    let date2 = new Date(time);     //这步是关键
+    let year = date2.getFullYear();
+    let mon = formatFunc(date2.getMonth() + 1);
+    let day = formatFunc(date2.getDate());
+
+    var dateStr = year+'年'+mon+'月'+day+'日';
+    alert(dateStr)
+    return dateStr;
+}
+
+export default UTC2GMT

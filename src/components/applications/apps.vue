9962f84145e5d641b9e039dcbbbbf3a08bb61841
OrangeM21 <orangemtony@gmail.com>
2019-11-15 15:34:56 +0800

    fixed

--- src/components/applications/apps.vue
+++ src/components/applications/apps.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
+  <div style="height: calc(100vh - 160px);">
     <div>{{ $route.params.id }}</div>
     <div>{{ $route.params.namespace }}</div>
+    <button @click="secret">secret</button>
+    <button @click="resource">resource</button>
+    <button @click="service">service</button>
   </div>
 </template>
 
 <script>
 import http from "../utils/httpAxios.js";
+import jsyaml from "js-yaml";
+
 export default {
   created: function() {
     this.url.url =
@@ -17,15 +22,44 @@ export default {
     console.log(this.url);
   },
   mounted: function() {
-    http(this.url).then(res => {
-      console.log(res);
-    });
+    this.getResources();
+  },
+  methods: {
+    resource() {
+      console.log(this.resources);
+    },
+    service() {
+      console.log(this.services);
+    },
+    secret() {
+      console.log(this.secrets);
+    },
+    getResources: async function() {
+      await http(this.url).then(res => {
+        let _this = this;
+        jsyaml.loadAll(res.data.data.manifest, function(doc) {
+          if (doc.kind == "Secret") {
+            _this.secrets.push({ name: doc.metadata.name, data: doc.data });
+          } else if (doc.kind == "Service") {
+            _this.services.push({ name: doc.metadata.name, kind: doc.kind });
+          } else {
+            _this.resources.push({ name: doc.metadata.name, kind: doc.kind });
+          }
+        });
+      });
+      // await http()
+    }
   },
   data() {
     return {
       url: {
         method: "get"
-      }
+      },
+      services: [],
+      secrets: [],
+      deployments: [],
+      resources: [],
+      installnation:{},
     };
   }
 };

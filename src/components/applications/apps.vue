b983ed9b316a117c0476f71c15b80ece6b66effb
OrangeM21 <orangemtony@gmail.com>
2019-11-18 18:27:28 +0800

    fixed

--- src/components/applications/apps.vue
+++ src/components/applications/apps.vue
@@ -1,26 +1,31 @@
 <template>
   <div style="height: calc(100vh - 160px);" class="main_page">
     <el-row>
-      <el-col :span="6">
-        <el-card class="box-card">
-          <div slot="header" class="clearfix">
-            <span>卡片名称</span>
-            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+      <el-col :span="4" :offset="2" >
+        <el-card :body-style="{ padding: '0px'}" style="text-align:left">
+          <div class="catalog-image">
+            <img v-show="catalog.icon" :src="catalog.icon" class="image" />
+            <img v-show="!catalog.icon" src="../../.././static/catalog/default.png" class="image" />
           </div>
-          <div v-for="o in 4" :key="o" class="text item">{{'列表内容 ' + o }}</div>
+          <div style="padding: 1em;">
+            <h5 class="catalog-label" style="font-size: 18px;">{{catalog.releaseName}}</h5>
+            <p class="catalog-label" style="font-size: 14px;">{{catalog.description}}</p>
+            <el-divider></el-divider>
+            <p class="label" style="font-size: 12px;">{{'App Version: ' + catalog.appv}}</p>
+            <p class="label" style="font-size: 12px;">{{'App Version: ' + catalog.chartv}}</p>
+          </div>
         </el-card>
       </el-col>
-      <el-col :span="18">
+      <el-col :span="14" :offset="2" style="text-align:left">
         <el-row>
-          <el-col :offset="2" :span="4">
-            <div class>
-              <el-button type="primary" @click="this.getdebug">主要按钮</el-button>
-            </div>
+          <el-col :span="3">
+            <el-button type="primary">Status</el-button>
           </el-col>
-          <el-col :offset="12" :span="4">
-            <div class>
-              <el-button type="success">成功按钮</el-button>
-            </div>
+          <el-col :offset="14" :span="3">
+            <el-button>Upgrade</el-button>
+          </el-col>
+          <el-col :offset="1" :span="3">
+            <el-button type="danger">Delete</el-button>
           </el-col>
         </el-row>
         <div v-show="this.AccessURLs!=''"></div>
@@ -28,14 +33,81 @@
           <h6>Access URLs</h6>
           <p>The current application does not expose a public URL.</p>
         </div>
-        <div v-show="this.secrets !=[]">
+        <div v-show="this.note != ''">
+          <h6>Notes</h6>
+          <section class="AppNotes Terminal elevation-1">
+            <div class="Terminal__Top type-small">
+              <div class="Terminal__Top__Buttons">
+                <span class="Terminal__Top__Button Terminal__Top__Button--red"></span>
+                <span class="Terminal__Top__Button Terminal__Top__Button--yellow"></span>
+                <span class="Terminal__Top__Button Terminal__Top__Button--green"></span>
+              </div>
+            </div>
+            <div class="Terminal__Tab">
+              <pre class="Terminal__Code">
+                <code>{{this.note}}
+              </code>
+            </pre>
+            </div>
+          </section>
+        </div>
+        <div v-show="this.secrets.length != 0">
           <h6>Secrets</h6>
-          <el-table :data="secrets" stripe style="width: 80%">
-            <el-table-column prop="name" label="NAME" width="180"></el-table-column>
-            <el-table-column prop="type" label="TYPE" width="180"></el-table-column>
-            <el-table-column prop="crt" label="CRT"></el-table-column>
-            <el-table-column prop="key" label="KEY"></el-table-column>
+          <el-table :data="secrets" stripe style="width: 100%">
+            <el-table-column prop="name" label="NAME"></el-table-column>
+            <el-table-column prop="type" label="TYPE"></el-table-column>
+            <el-table-column label="CRT">
+              <template slot-scope="scope">
+                <el-button
+                  @click.native.prevent="open(secrets[scope.$index].crt)"
+                  type="text"
+                  size="small"
+                >查看</el-button>
+              </template>
+            </el-table-column>
+            <el-table-column label="KEY">
+              <template slot-scope="scope">
+                <el-button
+                  @click.native.prevent="open(secrets[scope.$index].key)"
+                  type="text"
+                  size="small"
+                >查看</el-button>
+              </template>
+            </el-table-column>
           </el-table>
+        </div>
+        <div v-show="this.deployments.length != 0">
+          <h6>Deployments</h6>
+          <el-table :data="deployments" style="width: 100%">
+            <el-table-column prop="name" label="NAME"></el-table-column>
+            <el-table-column prop="desired" label="DESIRED"></el-table-column>
+            <el-table-column prop="update" label="UP-TO-DATE"></el-table-column>
+            <el-table-column prop="available" label="AVAILABLE"></el-table-column>
+          </el-table>
+        </div>
+        <div v-show="this.serviceDetail.length != 0">
+          <h6>Services</h6>
+          <el-table :data="serviceDetail" style="width: 100%">
+            <el-table-column prop="name" label="NAME"></el-table-column>
+            <el-table-column prop="type" label="TYPE"></el-table-column>
+            <el-table-column prop="cluster" label="CLUSTER-IP"></el-table-column>
+            <el-table-column prop="external" label="EXTERNAL-IP"></el-table-column>
+            <el-table-column prop="port" label="PORT(S)"></el-table-column>
+          </el-table>
+        </div>
+        <div v-show="this.resources.length!=0">
+          <h6>Other Resources</h6>
+          <el-table :data="resources" style="width: 100%">
+            <el-table-column prop="name" label="NAME"></el-table-column>
+            <el-table-column prop="kind" label="KIND"></el-table-column>
+          </el-table>
+        </div>
+        <div class="margin-t-normal">
+          <h6>{{'Installation Values'}}</h6>
+          <div class="ace-container">
+            <!-- 官方文档中使用 id，这里禁止使用，在后期打包后容易出现问题，使用 ref 或者 DOM 就行 -->
+            <div class="ace-editor" ref="ace"></div>
+          </div>
         </div>
       </el-col>
     </el-row>
@@ -43,9 +115,13 @@
 </template>
 
 <script>
+import ace from "ace-builds";
+import "ace-builds/webpack-resolver"; // 在 webpack 环境中使用必须要导入
+import "ace-builds/src-noconflict/theme-monokai"; // 默认设置的主题
+import "ace-builds/src-noconflict/mode-javascript"; // 默认设置的语言模式
 import http from "../utils/httpAxios.js";
 import jsyaml from "js-yaml";
-// import { Base64 } from 'js-base64';
+import { Base64 } from "js-base64";
 
 export default {
   created: function() {
@@ -63,18 +139,45 @@ export default {
     getdebug() {
       // console.log(this.services)
       // console.log(this.serviceDetail)
-      console.log(this.secrets);
+      console.log(this.deployments);
+    },
+    printdebug(data, log) {
+      // console.log(this.services)
+      // console.log(this.serviceDetail)
+      console.log(data, log);
     },
     getResources: async function() {
       await http(this.url).then(res => {
         let _this = this;
+        this.valuesYaml = res.data.data.config.raw;
+        this.catalog.description = res.data.data.chart.metadata.description;
+        this.catalog.icon = res.data.data.chart.metadata.icon;
+        this.catalog.appv = res.data.data.chart.metadata.appVersion;
+        this.catalog.releaseName = res.data.data.chart.metadata.name;
+        this.catalog.chartv = res.data.data.chart.metadata.version;
+        this.note = res.data.data.info.status.notes;
+        this.aceEditor = ace.edit(this.$refs.ace, {
+          maxLines: 30, // 最大行数，超过会自动出现滚动条
+          minLines: 10, // 最小行数，还未到最大行数时，编辑器会自动伸缩大小
+          fontSize: 14, // 编辑器内字体大小
+          theme: this.themePath, // 默认设置的主题
+          mode: this.modePath, // 默认设置的语言模式
+          value: this.valuesYaml ? this.valuesYaml : "",
+          tabSize: 4 // 制表符设置为 4 个空格大小
+        });
         jsyaml.loadAll(res.data.data.manifest, function(doc) {
           if (doc.kind == "Secret") {
-            _this.secrets.push({ name: doc.metadata.name,type:doc.type,key: doc.data[0],crt: doc.data[1] });
-            console.log()
+            console.log(doc.data);
+            _this.secrets.push({
+              name: doc.metadata.name,
+              type: doc.type,
+              key: Base64.decode(doc.data["tls.crt"]),
+              crt: Base64.decode(doc.data["tls.key"])
+            });
+            console.log();
           } else if (doc.kind == "Service") {
             _this.services.push({ name: doc.metadata.name, kind: doc.kind });
-          } else {
+          } else if (doc.kind != "Deployment") {
             _this.resources.push({ name: doc.metadata.name, kind: doc.kind });
           }
         });
@@ -89,7 +192,14 @@ export default {
           this.services[index].name;
         basicurl.method = "get";
         http(basicurl).then(res => {
-          console.log(res);
+          if (res.status == 200) {
+            this.deployments.push({
+              name: res.data.metadata["name"],
+              desired: res.data.status.replicas,
+              update: res.data.status.updatedReplicas,
+              available: res.data.status.availableReplicas
+            });
+          }
         });
       }
       //Services
@@ -102,9 +212,30 @@ export default {
           this.services[index].name;
         _basicurl.method = "get";
         http(_basicurl).then(res => {
-          console.log(res);
+          this.serviceDetail.push({
+            name: res.data.metadata["name"],
+            type: res.data.spec.type,
+            cluster: res.data.spec.clusterIP,
+            //TO_DO
+            external: "NONE",
+            port:
+              res.data.spec.ports[0].port +
+              "/" +
+              res.data.spec.ports[0].protocol
+          });
         });
       }
+    },
+    open(data) {
+      this.$alert(data, "密钥", {
+        confirmButtonText: "确定"
+        // callback: action => {
+        //   this.$message({
+        //     type: "info",
+        //     message: `action: ${action}`
+        //   });
+        // }
+      });
     }
   },
   data() {
@@ -119,7 +250,13 @@ export default {
       deploymentDetail: [],
       resources: [],
       installnation: {},
-      AccessURLs: ""
+      AccessURLs: "",
+      aceEditor: null,
+      valuesYaml: "",
+      note: "",
+      catalog: {},
+      themePath: "ace/theme/monokai", // 不导入 webpack-resolver，该模块路径会报错
+      modePath: "ace/mode/yaml" // 同上
     };
   }
 };
@@ -127,5 +264,99 @@ export default {
 <style scoped>
 .main_page {
   margin-top: 25px;
+}
+.Terminal {
+  background-color: #111a22;
+  border-radius: 5px;
+  padding: 0.625em 1.25em;
+}
+.type-small {
+  font-size: 0.8em;
+}
+.Terminal__Top {
+  height: 35px;
+  display: flex;
+  align-items: center;
+}
+
+.Terminal__Top__Title {
+  flex: 1;
+  text-align: center;
+  color: darkgray;
+}
+
+.Terminal__Top__Button {
+  border-radius: 15px;
+  display: inline-block;
+  height: 0.75em;
+  margin-right: 0.3125em;
+  width: 0.75em;
+}
+
+.Terminal__Top__Button--red {
+  background: #dd751d;
+}
+
+.Terminal__Top__Button--yellow {
+  background: #f9b479;
+}
+
+.Terminal__Top__Button--green {
+  background: #008145;
+}
+
+.Terminal__Tab {
+  padding-bottom: 0.625em;
+}
+
+.Terminal__Code {
+  margin: 0;
+  padding: 0;
+  white-space: pre-line;
+}
+
+.Terminal__Code code {
+  color: darkgray;
+}
+
+.Terminal__Code code.Terminal__Code__Command {
+  color: #f1f1f1;
+  display: inline-block;
+  margin-top: 1.25em;
+}
+
+.Terminal__Code code.Terminal__Code__Command:first-child {
+  margin-top: 0;
+}
+
+.Terminal__Code code.Terminal__Code__Command:before {
+  content: "$";
+  margin-right: 0.5em;
+}
+group::after,
+.el-table::before {
+  background-color: white !important;
+}
+.margin-t-normal {
+  margin-bottom: 3em;
+}
+.catalog-image {
+  display: flex;
+  height: calc(72px + 2em);
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  text-align: center;
+  background-color: #f1f1f1;
+  box-sizing: border-box;
+}
+.image {
+  max-width: 7em;
+  max-height: 6em;
+  display: block;
+  margin: 1em;
+}
+.label{
+
 }
 </style>

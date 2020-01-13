39afc11ec9c8ae1d9fee483573401f2fd51ed89f
maguohao <guohao.ma@fit2cloud.com>
2020-01-13 10:54:43 +0800

    更新仓库位置

--- dashboard/build.sh
+++ dashboard/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t registry.fit2cloud.com/kubeapps/kubeapps-plus:v1.0 .
-docker push registry.fit2cloud.com/kubeapps/kubeapps-plus:v1.0
+docker build -t registry.fit2cloud.com/public/docker.io/kubeoperator/kubeapps-plus/kubeapps-plus:v1.0 .
+docker push registry.fit2cloud.com/public/docker.io/kubeoperator/kubeapps-plus/kubeapps-plus:v1.0

4827e4019962b620cedd55e7aba11cafbbc8fd3d
Orange <orangemtony@gmail.com>
2020-01-19 11:22:37 +0800

    Update kubeappsctl_AI.sh

--- script/kubeappsctl_AI.sh
+++ script/kubeappsctl_AI.sh
@@ -170,7 +170,7 @@ function docker_upload_image() {
 
 function set_external_chartrepo() {
     repo_url=""
-    read_from_input repo_url "请输入Chart Repo的主机域名 例: http://chartmuseum.kubeapps.fit2cloud.com" "" "${repo_url}"
+    read_from_input repo_url "请输入Chart Repo的主机域名 例: https://registry.fit2cloud.com/chartrepo/operator，注意必须输入完整地址: http://$url/chartrepo/$project" "" "${repo_url}"
 
     repo_username=""
     read_from_input repo_username "请输入Repo的用户名" "" "${repo_username}"

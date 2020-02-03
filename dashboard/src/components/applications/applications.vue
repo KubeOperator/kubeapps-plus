7684d2faa195b7235c5742994d3441d5a65d4749
maguohao <guohao.ma@fit2cloud.com>
2020-02-03 15:44:50 +0800

    kubeapps-plus 图片丢失bug

--- dashboard/src/components/applications/applications.vue
+++ dashboard/src/components/applications/applications.vue
@@ -93,7 +93,7 @@
               <img v-else-if="(catalog.icon.search('gitlab')>=0)" src="../../assets/image/charts/gitlab-stack-110x117.png" class="image" require>
               <img v-else-if="(catalog.icon.search('istio')>=0)" src="../../assets/image/charts/istio-110x117.png" class="image" require>
               <img v-else-if="(catalog.icon.search('grafana')>=0)" src="../../assets/image/charts/grafana-stack-110x117.png" class="image" require>
-              <img v-else-if="(catalog.icon.search('kubeapps-plus')>=0)" src="../../assets/image/charts/kubeapps-plus-stack-110x117.png" class="image" require>
+              <img v-else-if="(catalog.icon.search('kubeapps')>=0)" src="../../assets/image/charts/kubeapps-plus-stack-110x117.png" class="image" require>
               <img v-else-if="(catalog.icon.search('loki')>=0)" src="../../assets/image/charts/loki-stack-110x117.png" class="image" require>
               <img v-else-if="(catalog.icon.search('prometheus')>=0)" src="../../assets/image/charts/prometheus-stack-110x117.png" class="image" require>
               <img v-else :src="catalog.icon" class="image" require>
